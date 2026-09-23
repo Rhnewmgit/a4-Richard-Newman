81f9eac8b365a6ecde9f3cb48d73f1a665cab8e1
Rhnewmgit <richienewm411@gmail.com>
2026-09-23 12:28:52 -0400

    Working frequency visualizer, audio playback

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,72 @@
-let selectedSong = 0;
+const audioPlr = document.querySelector(".audioPlayerContainer").querySelector("audio");
+audioPlr.src = "./audio/Carefree.mp3";
 
-window.onload = function () {
+const canvas = document.querySelector(".audioDisplayContainer").querySelector("canvas");
+const canvasCtx = canvas.getContext("2d");
+canvas.width = 1024;
+canvas.height = 256;
+canvasCtx.fillStyle = "#13171f";
+canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
+function start() {
+	const audioCtx = new AudioContext();
+
+	const analyser = audioCtx.createAnalyser(); // british spelling!
+	analyser.fftSize = 2048; // 1024 bins
+	const bufferLength = analyser.frequencyBinCount;
+
+	canvas.width = bufferLength;
+	canvas.height = 256;
+
+	const player = audioCtx.createMediaElementSource(audioPlr);
+	player.connect(audioCtx.destination);
+	player.connect(analyser);
+	const results = new Uint8Array(bufferLength);
+	function draw() {
+		window.requestAnimationFrame(draw);
+
+		// Clear canvas
+		canvasCtx.fillStyle = "#10141b";
+		canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+		canvasCtx.fillStyle = "white";
+
+		analyser.getByteFrequencyData(results);
+		for (let i = 0; i < bufferLength; i++) {
+			canvasCtx.fillRect(i, canvas.height - results[i], 1, results[i]);
+		}
+	}
+
+	draw();
+}
+
+window.onclick = () => {
+	start();
+	audioPlr.style.pointerEvents = "all";
+	window.onclick = null;
 };
+
+let inputAudioUrl;
+
+document.querySelector("#audioFileInput").addEventListener("change", (e) => {
+	const file = e.target.files[0];
+	if (!file) {
+		return;
+	}
+	if (inputAudioUrl) {
+		URL.revokeObjectURL(inputAudioUrl);
+	}
+	inputAudioUrl = URL.createObjectURL(file);
+	audioPlr.src = inputAudioUrl;
+	audioPlr.load();
+});
+
+const audioSelector = document.querySelector("#audioSelector");
+audioSelector.addEventListener("change", () => {
+	const selected = audioSelector.value;
+	if (selected === "upload") {
+		audioPlr.src = inputAudioUrl;
+	} else {
+		audioPlr.src = `./audio/${selected}`;
+	}
+	audioPlr.play();
+});
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-require("dotenv").config();
-
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/public/index.html");
 });
-
 
 app.listen(process.env.PORT || 3000);
